54b64783bccc83e3d611988424b5f731e54e133e
Stremaine-sept-2022 <gc.ahbc.sept.2022@gmail.com>
2023-03-02 19:59:44 -0500

    Basic Loop Lab

---
+++ LabsForSept2022/BasicLoops/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+do
+{
+    Console.WriteLine("Hello, World!");
+    Console.Write("Would you like to continue (y/n)?");
+    string answer = Console.ReadLine().ToLower().Trim();
+    if (answer == "n")
+    {
+        break;
+    }
+
+}while (true);
+
